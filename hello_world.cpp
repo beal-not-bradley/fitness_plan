109a2949d4f2675434326cf0255885a645af8c57
tyler-beal <tybeal4@gmail.com>
2021-08-21 15:53:01 -0700

    Add hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	//print output to user
+	cout << "Hello World!" << endl;
+	return 0;
+}
+
+//compiling a program in the terminal: g++ hello_wrold.cpp
+//running the binary filed produced from compiling to get output: ./a.out
+
+//can name binary output through terminal: g++ hello_world -o hello_world
+//running that binary file: ./hello_world
